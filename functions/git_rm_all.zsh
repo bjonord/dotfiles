d85a719872e726fb560cc3cf0ddeb99cc480f525
bjoska <bjorn.skarner@it.cdon.com>
2013-11-20 10:48:36 +0100

    minor changes.

--- functions/git_rm_all.zsh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-function git_rm_all(){
-    git rm `git status | grep deleted | awk '{print $3}'`
-}
-git_rm_all
--- git.zsh
+++ git.zsh
@@ -10,4 +10,4 @@ alias 'gstat=git status'
 alias 'gdiffc=git diff --cached'
 alias 'gdiff=git diff'
 alias "glg=git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
-alias "gitrma2=git status | grep deleted | awk '{print $3}' | xargs git rm"
+alias gitrma="git status | grep deleted | awk '{print $3}' | xargs git rm"
