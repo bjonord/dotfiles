06bd152e88cd6eea6424f2b22448b922549adb86
bjoska <bjorn.skarner@it.cdon.com>
2014-01-23 10:34:47 +0100

    addition of confirurations for openbox, thunar, tint2 and conky.

--- git.zsh
+++ git.zsh
@@ -11,4 +11,4 @@ alias 'gdiffc=git diff --cached'
 alias 'gdiff=git diff'
 alias "glg=git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 alias gitrma="sh -c 'git ls-files --deleted | xargs -r git rm'"
-alias gstu = !"git log --reverse --branches --since='$(if [[ "Mon" == "$(date +%a)" ]]; then echo "last friday"; else echo "yesterday"; fi)' --author=$(git config --get user.email) --format=format:'%C(cyan) %ad %C(yellow)%h %Creset %s %Cgreen%d' --date=local"
+alias "gstu=git log --reverse --branches --since='$(if [[ "Mon" == "$(date +%a)" ]]; then echo "last friday"; else echo "yesterday"; fi)' --author=$(git config --get user.email) --format=format:'%C(cyan) %ad %C(yellow)%h %Creset %s %Cgreen%d' --date=local"
