26389860e5f7683c38651172f1496ef8b88bafca
bjoska <bjorn.skarner@it.cdon.com>
2014-05-16 13:34:15 +0200

    Add new git blame alias(because why not).

--- zsh.d/git.zsh
+++ zsh.d/git.zsh
@@ -13,3 +13,4 @@ alias 'gpr=git pull --rebase'
 alias "glg=git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 alias gitrma="sh -c 'git ls-files --deleted | xargs -r git rm'"
 alias "gstu=git log --reverse --branches --since='$(if [[ "Mon" == "$(date +%a)" ]]; then echo "last friday"; else echo "yesterday"; fi)' --author=$(git config --get user.email) --format=format:'%C(cyan) %ad %C(yellow)%h %Creset %s %Cgreen%d' --date=local"
+alias 'wtf=git blame'
